3cfd6b16fcda1f1c2115af96ece26097f9ec466a
YogaPan <godhand1234567@gmail.com>
2016-02-18 11:17:01 +0000

    Add git-email

--- build-install.sh
+++ build-install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install gcc libssl-dev libncurses5-dev
 
 # Install mail-utils.
-sudo apt-get install mutt esmtp
+sudo apt-get install mutt esmtp git-email
 
 # pip3
 sudo apt-get install python3-pip
