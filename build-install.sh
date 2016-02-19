ea5bad9a2286b210b3448d7342776310bb4ce73b
yourname <username@gmail.com>
2016-02-19 09:16:29 +0000

    Add htop

--- build-install.sh
+++ build-install.sh
@@ -1,6 +1,7 @@
 # Install build-essential.
 sudo apt-get update
 sudo apt-get install gcc libssl-dev libncurses5-dev
+sudo apt-get install htop
 
 # Install mail-utils.
 sudo apt-get install mutt esmtp git-email
