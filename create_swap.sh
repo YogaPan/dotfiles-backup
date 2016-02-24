79b5ef9fac5bc0857b5ef8b76b921616ad4efd54
YogaPan <godhand1234567@gmail.com>
2016-02-25 01:12:03 +0800

    Add blank line

--- create_swap.sh
+++ create_swap.sh
@@ -2,6 +2,7 @@ sudo dd if=/dev/zero of=/swapfile bs=1024 count=524288
 sudo mkswap /swapfile
 sudo swapon /swapfile
 grep "swapfile" /etc/fstab || sudo echo "/swapfile none swap sw 0 0" >> /etc/fstab
-cat /etc/fstab
-swapon -s
+echo '\n'
+cat /etc/fstab; echo '\n'
+swapon -s; echo '\n'
 free -m
