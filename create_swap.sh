371b6b29be06e759d5c4b939ae416c3ba40a6399
YogaPan <godhand1234567@gmail.com>
2016-02-25 01:08:05 +0800

    show all swapon

--- create_swap.sh
+++ create_swap.sh
@@ -2,5 +2,6 @@ sudo dd if=/dev/zero of=/swapfile bs=1024 count=524288
 sudo mkswap /swapfile
 sudo swapon /swapfile
 grep "swapfile" /etc/fstab || sudo echo "/swapfile none swap sw 0 0" >> /etc/fstab
-free -m
 cat /etc/fstab
+swapon -s
+free -m
