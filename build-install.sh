d02a2dedee3794d1700f187eac75b7da3a51978b
YogaPan <godhand1234567@gmail.com>
2016-02-25 00:55:33 +0800

    Add compile tool

--- build-install.sh
+++ build-install.sh
@@ -1,10 +1,14 @@
 # Install build-essential.
 sudo apt-get update
-sudo apt-get install gcc libssl-dev libncurses5-dev
-sudo apt-get install htop
+sudo apt-get install gcc g++ make cmake
+sudo apt-get install python-dev
+sudo apt-get install libssl-dev libncurses5-dev
 
 # Install mail-utils.
-sudo apt-get install mutt esmtp git-email
+sudo apt-get install mutt esmtp
+
+# Fancy top.
+sudo apt-get install htop
 
 # pip3
 sudo apt-get install python3-pip
