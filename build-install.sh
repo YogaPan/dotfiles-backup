693b0fb26ebe80097690a82f91ee24c0f7f2cd4f
YogaPan <godhand1234567@gmail.com>
2016-04-03 20:57:22 +0800

    Add debug tool

--- build-install.sh
+++ build-install.sh
@@ -1,6 +1,7 @@
 # Install build-essential.
 sudo apt-get update
-sudo apt-get install gcc g++ make cmake gdb
+sudo apt-get install gcc g++ make cmake
+sudo apt-get install gdb valgrind
 sudo apt-get install python-dev
 sudo apt-get install libssl-dev libncurses5-dev
 
