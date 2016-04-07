831843f34b7f1ef9ce60f27e729685601c36fc20
YogaPan <godhand1234567@gmail.com>
2016-04-07 23:42:05 +0800

    Add cloc and tree

--- build-install.sh
+++ build-install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Install build-essential.
 sudo apt-get update
 sudo apt-get install gcc g++ make cmake
@@ -5,18 +6,17 @@ sudo apt-get install gdb valgrind
 sudo apt-get install libssl-dev libncurses5-dev
 sudo apt-get install software-properties-common
 
+# Install mail-utils.
+sudo apt-get install mutt esmtp
+
 # For coding sytle and document.
 sudo apt-get install astyle
 sudo apt-get install doxygen
 
-# Install mail-utils.
-sudo apt-get install mutt esmtp
-
-# Fancy top.
 sudo apt-get install htop
-
-# For git log
 sudo apt-get install tig
+sudo apt-get install cloc
+sudo apt-get install tree
 
 # pip
 sudo apt-get install python-dev python3-dev python-pip python3-pip
@@ -24,10 +24,3 @@ sudo apt-get install python-dev python3-dev python-pip python3-pip
 # Python web
 sudo pip3 install requests
 sudo pip3 install django
-
-# Python science
-sudo pip3 install numpy
-sudo apt-get install python3-matplotlib
-sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
-sudo pip3 install scipy
-sudo pip3 install sklearn
