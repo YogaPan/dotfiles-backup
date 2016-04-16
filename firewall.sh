f01f14c1584491dac90dc42dba7b64d55a9c3c2b
YogaPan <godhand1234567@gmail.com>
2016-04-16 23:28:10 +0800

    Add FTP

--- firewall.sh
+++ firewall.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 sudo iptables -F
-sudo iptables -I INPUT -i lo -j ACCEPT
+
+# sudo iptables -I INPUT -i lo -j ACCEPT
 sudo iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A INPUT -p icmp -j ACCEPT
+sudo iptables -I INPUT -p icmp -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 21 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 50000:60000 -j ACCEPT
 sudo iptables -A INPUT -j REJECT
+
+sudo iptables -nL
