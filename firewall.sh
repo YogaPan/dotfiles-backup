6867e6fd60614ef08616cd41d9a4f69fd0222a94
YogaPan <godhand1234567@gmail.com>
2016-04-17 21:25:01 +0800

    Fix

--- firewall.sh
+++ firewall.sh
@@ -3,19 +3,17 @@
 sudo iptables -F
 
 # limit
-# sudo iptables -A INPUT -m connlimit --connlimit-above 100 -j REJECT
+sudo iptables -I INPUT -m connlimit --connlimit-above 100 -j REJECT
 sudo iptables -A INPUT -p icmp -m limit --limit 1/m --limit-burst 10 -j ACCEPT
 sudo iptables -A INPUT -p icmp -j DROP
 
 # open ports
 sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 21 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m multiport --dports 21,22,8080 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 50000:60000 -j ACCEPT
 
 # enable localhost
-sudo iptables -I INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
 
 # reject not match pattern
 sudo iptables -A INPUT -j REJECT
