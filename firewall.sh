8fc116b4e683bc4f5abdf9a4d3167a518d31967f
YogaPan <godhand1234567@gmail.com>
2016-04-17 17:45:15 +0800

    Add iptables limit

--- firewall.sh
+++ firewall.sh
@@ -2,13 +2,23 @@
 
 sudo iptables -F
 
-# sudo iptables -I INPUT -i lo -j ACCEPT
-sudo iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -I INPUT -p icmp -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 21 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 50000:60000 -j ACCEPT
+# limit
+# sudo iptables -A INPUT -m connlimit --connlimit-above 100 -j REJECT
+sudo iptables -A INPUT -p icmp -m limit --limit 1/m --limit-burst 10 -j ACCEPT
+sudo iptables -A INPUT -p icmp -j DROP
+
+# open ports
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 50000:60000 -j ACCEPT
+
+# enable localhost
+sudo iptables -I INPUT -i lo -j ACCEPT
+
+# reject not match pattern
 sudo iptables -A INPUT -j REJECT
 
+# show informations
 sudo iptables -nL
