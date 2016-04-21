6e79243e5e99a8588e8e0de874fda7a3305aa2d6
YogaPan <godhand1234567@gmail.com>
2016-04-21 22:46:32 +0800

    Add ubuntu desktop

---
+++ desktop-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install guake
+sudo apt-get install fcitx
+sudo apt-get install flashplugin-installer
+sudo apt-get install unity-tweak-tool
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-icon-theme-circle numix-gtk-theme
