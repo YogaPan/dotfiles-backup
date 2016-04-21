5ac499cb0b98a6e5b2e1ca0199fe2b01af7d631e
YogaPan <godhand1234567@gmail.com>
2016-04-21 23:01:35 +0800

    Add music

--- desktop-install.sh
+++ desktop-install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install guake
 sudo apt-get install fcitx
+sudo apt-get install guake
+apt-get install fonts-hack-ttf
 sudo apt-get install flashplugin-installer
+
 sudo apt-get install unity-tweak-tool
 sudo add-apt-repository ppa:numix/ppa
 sudo apt-get update
 sudo apt-get install numix-icon-theme-circle numix-gtk-theme
+
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install pulseaudio-equalizer
