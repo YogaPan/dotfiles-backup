6f3d78d6c7caf557a51a12b833560ec2161816df
YogaPan <godhand1234567@gmail.com>
2016-04-22 19:43:12 +0800

    Add '~' and fix -y options

--- desktop-install.sh
+++ desktop-install.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-sudo apt-get install fcitx
-sudo apt-get install guake
-apt-get install fonts-hack-ttf
-sudo apt-get install flashplugin-installer
+sudo apt-get install fcitx -y
+sudo apt-get install guake -y
+sudo apt-get install fonts-hack-ttf -y
+sudo apt-get install flashplugin-installer -y
 
-sudo apt-get install unity-tweak-tool
+sudo apt-get install unity-tweak-tool -y
 sudo add-apt-repository ppa:numix/ppa
 sudo apt-get update
-sudo apt-get install numix-icon-theme-circle numix-gtk-theme
+sudo apt-get install numix-icon-theme-circle numix-gtk-theme -y
 
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
-sudo apt-get install pulseaudio-equalizer
+sudo apt-get install pulseaudio-equalizer -y
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
+  | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ \
+  stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get install google-chrome-stable -y
+
+# In /usr/share/X11/xkb/symbols/pc
+#
+# key <LSGT> {    [ less, greater, bar, brokenbar  ]  };
+# key <LSGT> { [ grave, asciitilde, grave, asciitilde  ]  };
