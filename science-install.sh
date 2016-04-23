722db93619fd95ef8b0ce5fb4b1675279f91d7a6
YogaPan <godhand1234567@gmail.com>
2016-04-23 16:27:12 +0800

    pip3 without sudo

--- science-install.sh
+++ science-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Python science
-sudo pip3 install numpy
+pip3 install numpy
 sudo apt-get install python3-matplotlib -y
-sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran -y
-sudo pip3 install scipy
-sudo pip3 install sklearn
+pip3 install scipy
+pip3 install sklearn
