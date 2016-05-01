2d00b44350bf180b420091a8f507c3c9a594eb7a
YogaPan <godhand1234567@gmail.com>
2016-05-01 15:34:27 +0800

    delete trailing space

--- desktop-install.sh
+++ desktop-install.sh
@@ -15,10 +15,10 @@ sudo apt-get update
 sudo apt-get install pulseaudio-equalizer -y
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
-  | sudo apt-key add - 
+  | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ \
   stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install google-chrome-stable -y
 
 # sudo add-apt-repository "deb http://archive.canonical.com/ \
