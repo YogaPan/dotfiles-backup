086c160b560e71fe82813b98aa051d01a55bd850
YogaPan <godhand1234567@gmail.com>
2016-05-02 01:44:22 +0800

    Add mac install

---
+++ mac-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+xcode-select --install
+
+/usr/bin/ruby -e "$(curl -fsSL
+https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install cmake
+brew install python3
+
+brew install tree
+brew install cloc
+brew install htop
+brew install nload
+brew install tmux
+brew install nmap
+brew install sqlmap
+brew install apache2-utils
