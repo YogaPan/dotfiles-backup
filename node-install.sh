514966e8afcaa56da18960691437d3675420aa7b
YogaPan <godhand1234567@gmail.com>
2016-05-01 15:25:32 +0800

    node add mac os dependencies

--- node-install.sh
+++ node-install.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash -
-sudo apt-get install nodejs -y
+# Detect operating system
+operating_system=`uname`
 
-sudo npm install -g bower
-sudo npm install -g speed-test
-sudo npm install -g gulp
-sudo npm install -g live-server
+if [ $operating_system = "Darwin" ]; then
+  echo "Install Mac os dependencies...\n"
+  brew install node
+fi
+
+if [ $operating_system = "Linux" ]; then
+  echo "Install Linux dependencies...\n"
+  apt-get install curl -y
+  curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash -
+  apt-get install nodejs -y
+fi
+
+npm install -g bower
+npm install -g speed-test
+npm install -g gulp
+npm install -g live-server
 
 rm -rf ~/.tldr
 mkdir ~/.tldr
-sudo npm install -g tldr
+npm install -g tldr
