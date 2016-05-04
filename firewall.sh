9130de99eacfe14c8d0ff1423d3c6d0336f385b8
YogaPan <godhand1234567@gmail.com>
2016-05-05 00:29:36 +0800

    delete sudo

--- firewall.sh
+++ firewall.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-sudo iptables -F
+iptables -F
 
 # limit
-sudo iptables -I INPUT -m connlimit --connlimit-above 100 -j REJECT
-sudo iptables -A INPUT -p icmp -m limit --limit 1/m --limit-burst 10 -j ACCEPT
-sudo iptables -A INPUT -p icmp -j DROP
+iptables -I INPUT -m connlimit --connlimit-above 100 -j REJECT
+iptables -A INPUT -p icmp -m limit --limit 1/m --limit-burst 10 -j ACCEPT
+iptables -A INPUT -p icmp -j DROP
 
 # open ports
-sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A INPUT -p tcp -m multiport --dports 21,22,8080 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 50000:60000 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 21,22,8080 -j ACCEPT
+iptables -A INPUT -p tcp --dport 50000:60000 -j ACCEPT
 
 # enable localhost
-sudo iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
 
 # reject not match pattern
-sudo iptables -A INPUT -j REJECT
+iptables -A INPUT -j REJECT
 
 # show informations
-sudo iptables -nL
+iptables -nL
--- linux-install.sh
+++ linux-install.sh
@@ -2,31 +2,31 @@
 
 # Install build-essential.
 # sudo apt-get install software-properties-common
-sudo apt-get update
-sudo apt-get install build-essential -y
-sudo apt-get install gdb valgrind -y
-sudo apt-get install libssl-dev libncurses5-dev -y
+apt-get update
+apt-get install build-essential -y
+apt-get install gdb valgrind -y
+apt-get install libssl-dev libncurses5-dev -y
 
 # For perf
-sudo apt-get install linux-tools-common -y
-sudo apt-get install linux-tools-`uname -r` linux-cloud-tools-`uname -r` -y
+apt-get install linux-tools-common -y
+apt-get install linux-tools-`uname -r` linux-cloud-tools-`uname -r` -y
 
 # For coding sytle and document.
-sudo apt-get install astyle -y
-sudo apt-get install doxygen -y
+apt-get install astyle -y
+apt-get install doxygen -y
 
 # Useful tools
-sudo apt-get install tmux -y
-sudo apt-get install htop -y
-sudo apt-get install nload -y
-sudo apt-get install tig -y
-sudo apt-get install cloc -y
-sudo apt-get install tree -y
-sudo apt-get install gnuplot -y
+apt-get install tmux -y
+apt-get install htop -y
+apt-get install nload -y
+apt-get install tig -y
+apt-get install cloc -y
+apt-get install tree -y
+apt-get install gnuplot -y
 
 # Web Tools
-sudo apt-get install nginx -y
-sudo apt-get install vsftpd -y
+apt-get install nginx -y
+apt-get install vsftpd -y
 
 sudo apt-get install apache2-utils -y
 sudo add-apt-repository ppa:wireshark-dev/stable -y
